e9f4880aa994e3956d7f7ad389a4be3a27be640d
thinhnt-vn <thinhnt.vn@gmail.com>
2017-03-15 13:22:06 +0700

    Upload RMI Interfaces

---
+++ bkstorage/BKStorage.Interface/src/hust/soict/bkstorage/remoteentity/Package.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hust.soict.bkstorage.remoteentity;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author toant_000
+ * @param <T>
+ */
+public class Package <T> implements Serializable{
+    private final int id;        // id của người đăng nhập. trả lại -1 nếu đăng nhập thất bại
+    private final T data;           // Nội dung
+
+    public Package(int id, T data) {
+        this.id = id;
+        this.data = data;
+    }
+
+    public int getID() {
+        return id;
+    }
+
+    public T getData() {
+        return data;
+    }
+    
+}
